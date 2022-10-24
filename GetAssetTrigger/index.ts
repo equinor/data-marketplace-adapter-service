a358a7a32b856e85a708eebf41f2690c57929b57
Aron Høyer <aron@hoyer.com>
2022-10-24 12:51:06 +0200

    feat: Get asset from Collibra

--- GetAssetTrigger/index.ts
+++ GetAssetTrigger/index.ts
@@ -1,20 +1,60 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import axios from "axios";
-import {config} from "../config";
+import axios, { AxiosError } from "axios"
+import type { Asset } from "@equinor/data-marketplace-models"
+import { config } from "../config"
 
 const GetAssetTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const { id } = context.bindingData
+  const { id } = context.bindingData
 
-    const collibraAssetRes = axios.get(`${config.COLLIBRA_BASE_URL}/${id}`)
-    context.log("[GetAssetTrigger] id", id)
+  try {
+    const { data } = await axios.get(`${config.COLLIBRA_BASE_URL}/assets/${id}`, {
+      headers: {
+        authorization: req.headers.authorization,
+      },
+    })
+
+    const asset: Asset = {
+      createdAt: new Date(data.createdOn),
+      description: "", // TODO: get description
+      excerpt: "",
+      id: data.id,
+      provider: {
+        id: "",
+        name: "Collibra",
+      },
+      qualityScore: 0,
+      rating: 0,
+      tags: [],
+      type: {
+        id: data.type.id,
+        name: data.type.name,
+      },
+      updatedAt: new Date(data.lastModifiedOn),
+      updateFrequency: "", // TODO: get timeliness
+    }
 
     context.res = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({message: "Hello, World"})
-    };
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(asset),
+    }
+  } catch (e) {
+    const _e = e as AxiosError
 
-};
+    context.log(`Request to ${_e.config.url} failed with status code ${_e.response.status}`)
+    context.log(_e.response.data)
 
-export default GetAssetTrigger;
+    context.res = {
+      status: _e.status ?? 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        error: _e.message,
+      }),
+    }
+  }
+}
+
+export default GetAssetTrigger
