7307ada7dee619f1a8b23cc2ccca10045f052dcd
Aron Høyer <aron@hoyer.com>
2022-11-03 15:24:29 +0100

    refactor: No `let` :angry:

--- lib/collibra/rights_to_use_adapter.ts
+++ lib/collibra/rights_to_use_adapter.ts
@@ -1,46 +1,19 @@
-import { RightsToUse } from "@equinor/data-marketplace-models"
+import type { RightsToUse } from "@equinor/data-marketplace-models"
+import type { Attribute } from "@equinor/data-marketplace-models/types/Attribute"
 import { htmlToPortableText } from "../html_to_portable_text"
 
 export const rightsToUseAdapter = (asset: Collibra.AssetWithAttributes): RightsToUse => {
+  const emptyAttr: Attribute = {
+    id: "",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    value: "",
+    name: "",
+  }
+
   const descriptionAttr = asset.attributes.find((attr) => attr.type.name.toLowerCase() === "description")
-
   const authURLattr = asset.attributes.find((attr) => attr.type.name.toLowerCase() === "authorization url")
-  let authURL: RightsToUse["authURL"] = {
-    id: "",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    value: "",
-    name: "",
-  }
-
-  if (authURLattr) {
-    authURL = {
-      ...authURL,
-      id: authURLattr.id,
-      createdAt: new Date(authURLattr.createdOn),
-      updatedAt: new Date(authURLattr.lastModifiedOn),
-      value: htmlToPortableText(authURLattr.value),
-    }
-  }
-
   const termsAttr = asset.attributes.find((attr) => attr.type.name.toLowerCase() === "terms and conditions")
-  let terms: RightsToUse["terms"] = {
-    id: "",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    value: "",
-    name: "",
-  }
-
-  if (termsAttr) {
-    terms = {
-      ...terms,
-      id: termsAttr.id,
-      createdAt: new Date(termsAttr.createdOn),
-      updatedAt: new Date(termsAttr.lastModifiedOn),
-      value: htmlToPortableText(termsAttr.value),
-    }
-  }
 
   return {
     id: asset.id,
@@ -48,7 +21,23 @@ export const rightsToUseAdapter = (asset: Collibra.AssetWithAttributes): RightsT
     createdAt: new Date(asset.createdOn),
     updatedAt: new Date(asset.lastModifiedOn),
     description: descriptionAttr ? htmlToPortableText(descriptionAttr.value) : [],
-    authURL,
-    terms,
+    authURL: authURLattr
+      ? {
+          createdAt: new Date(authURLattr.createdOn),
+          id: authURLattr.id,
+          name: "",
+          updatedAt: new Date(authURLattr.lastModifiedOn),
+          value: authURLattr.value as string,
+        }
+      : emptyAttr,
+    terms: termsAttr
+      ? {
+          createdAt: new Date(termsAttr.createdOn),
+          id: termsAttr.id,
+          name: "",
+          updatedAt: new Date(termsAttr.lastModifiedOn),
+          value: termsAttr.value as string,
+        }
+      : emptyAttr,
   }
 }
