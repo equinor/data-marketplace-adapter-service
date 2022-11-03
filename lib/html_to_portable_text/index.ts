b4f77b524f8aa85265cc4c2d39762f41e32767af
Aron Høyer <aron@hoyer.com>
2022-11-03 15:15:10 +0100

    refactor: Remove superfluous explicit return

--- lib/html_to_portable_text/index.ts
+++ lib/html_to_portable_text/index.ts
@@ -6,9 +6,8 @@ import { rules } from "./rules"
 import { sanitizeHtml } from "../sanitize_html"
 import { htmlStringToDom } from "../html_string_to_dom"
 
-export const htmlToPortableText = (html: string): PortableTextBlock[] => {
-  return htmlToBlocks(html, collibraRichText.get("content").fields.find((field) => field.name === "body").type, {
+export const htmlToPortableText = (html: string): PortableTextBlock[] =>
+  htmlToBlocks(html, collibraRichText.get("content").fields.find((field) => field.name === "body").type, {
     rules,
     parseHtml: (html: string) => htmlStringToDom(sanitizeHtml(html)),
   })
-}
