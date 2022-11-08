fc730d5a24e13ff87569553ee99db23d57f3a171
Aron Høyer <aron@hoyer.com>
2022-11-08 15:08:12 +0100

    fix: Respond with status code if error

--- GetAssetTrigger/index.ts
+++ GetAssetTrigger/index.ts
@@ -13,45 +13,35 @@ const GetAssetTrigger: AzureFunction = async function (context: Context, req: Ht
   const { id } = context.bindingData
   const collibraClient = makeCollibraClient({ headers: { authorization: req.headers.authorization } })
 
-  try {
-    const asset = await pipe(
-      getAssetByID(collibraClient)(id),
-      TE.bindTo("collibraAsset"),
-      TE.bind("collibraAttributes", () => getAssetAttributes(collibraClient)(id)),
-      TE.map(({ collibraAsset, collibraAttributes }) =>
-        assetAdapter({ ...collibraAsset, attributes: collibraAttributes })
-      ),
-      TE.match<AxiosError, Net.Result<Asset>, Asset>(
-        (err: AxiosError) => ({
-          status: err.response.status,
-          statusText: err.response.statusText,
-          error: err.message,
-          value: null,
-        }),
-        (collibraAsset) => ({
-          status: 200,
-          statusText: "OK",
-          error: null,
-          value: collibraAsset,
-        })
-      )
-    )()
+  const res = await pipe(
+    getAssetByID(collibraClient)(id),
+    TE.bindTo("collibraAsset"),
+    TE.bind("collibraAttributes", () => getAssetAttributes(collibraClient)(id)),
+    TE.map(({ collibraAsset, collibraAttributes }) =>
+      assetAdapter({ ...collibraAsset, attributes: collibraAttributes })
+    ),
+    TE.match<AxiosError, Net.Result<Asset>, Asset>(
+      (err: AxiosError) => ({
+        status: err.response.status,
+        statusText: err.response.statusText,
+        error: err.message,
+        value: null,
+      }),
+      (collibraAsset) => ({
+        status: 200,
+        statusText: "OK",
+        error: null,
+        value: collibraAsset,
+      })
+    )
+  )()
 
-    context.res = {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(asset),
-    }
-  } catch (error) {
-    console.error(error)
-    context.res = {
-      status: error.status,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ error }),
-    }
+  context.res = {
+    status: res.status,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(res.error ? { error: res.error } : res.value),
   }
 }
 
--- lib/collibra/client/get_asset_by_id.ts
+++ lib/collibra/client/get_asset_by_id.ts
@@ -1,11 +1,11 @@
 import * as TE from "fp-ts/TaskEither"
 import * as E from "fp-ts/Either"
-import { AxiosInstance } from "axios"
+import { AxiosInstance, AxiosResponse } from "axios"
 import { pipe } from "fp-ts/lib/function"
 
-export const getAssetByID = (client: AxiosInstance) => (id: string) =>
+export const getAssetByID = (client: Net.Client) => (id: string) =>
   pipe(
-    TE.tryCatch(() => client.request<Collibra.Asset>({ url: `/assets/${id}` }), E.toError),
+    TE.tryCatch(() => client.get<AxiosResponse<Collibra.Asset>>(`/assets/${id}`), E.toError),
     TE.bindTo("res"),
     TE.map(({ res }) => res.data)
   )
