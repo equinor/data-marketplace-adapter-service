9543c32fdd0e2c7b07009ab3da91635f6978cd96
Aron Høyer <aron@hoyer.com>
2022-11-14 12:56:47 +0100

    refactor: Move net calls into their own functions

--- lib/collibra/client/get_rights_to_use_asset.ts
+++ lib/collibra/client/get_rights_to_use_asset.ts
@@ -4,35 +4,32 @@ import * as TE from "fp-ts/TaskEither"
 import { pipe } from "fp-ts/lib/function"
 
 type RelationTypesResponse = AxiosResponse<Collibra.PagedRelationTypeResponse>
+const getRelationTypes = (client: Net.Client) => () =>
+  client.get<RelationTypesResponse>("/relationTypes", {
+    params: new URLSearchParams({
+      sourceTypeName: "data product",
+      targetTypeName: "rights-to-use",
+    }),
+  })
+
 type RelationsResponse = AxiosResponse<Collibra.PagedRelationResponse>
+const getRelations = (client: Net.Client, relationTypeID: string, id: string) => () =>
+  client.get<RelationsResponse>("/relations", {
+    params: new URLSearchParams({
+      relationTypeId: relationTypeID,
+      sourceId: id,
+    }),
+  })
+
 type AssetResponse = AxiosResponse<Collibra.Asset>
+const getAsset = (client: Net.Client, id: string) => () => client.get<AssetResponse>(`/assets/${id}`)
 
 export const getRightsToUseAsset = (client: Net.Client) => (id: string) =>
   pipe(
-    TE.tryCatch(
-      () =>
-        client.get<RelationTypesResponse>("/relationTypes", {
-          params: new URLSearchParams({
-            sourceTypeName: "data product",
-            targetTypeName: "rights-to-use",
-          }),
-        }),
-      E.toError
+    TE.tryCatch(getRelationTypes(client), E.toError),
+    TE.chain(({ data: relationTypesRes }) =>
+      TE.tryCatch(getRelations(client, relationTypesRes.results[0].id, id), E.toError)
     ),
-    TE.chain(({ data }) =>
-      TE.tryCatch(
-        () =>
-          client.get<RelationsResponse>("/relations", {
-            params: new URLSearchParams({
-              relationTypeId: data.results[0].id,
-              sourceId: id,
-            }),
-          }),
-        E.toError
-      )
-    ),
-    TE.chain(({ data }) =>
-      TE.tryCatch(() => client.get<AssetResponse>(`/assets/${data.results[0].target.id}`), E.toError)
-    ),
+    TE.chain(({ data: relationsRes }) => TE.tryCatch(getAsset(client, relationsRes.results[0].target.id), E.toError)),
     TE.map(({ data }) => data)
   )
