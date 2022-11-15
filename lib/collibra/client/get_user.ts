90c13571e61b96344090d500128548d7ffd58a5e
Aron Høyer <aron@hoyer.com>
2022-11-15 15:46:45 +0100

    fix: Fix 404 from Collibra

--- lib/collibra/client/get_user.ts
+++ lib/collibra/client/get_user.ts
@@ -3,8 +3,8 @@ import * as E from "fp-ts/Either"
 import * as TE from "fp-ts/TaskEither"
 import { pipe } from "fp-ts/function"
 
-type UserResponse = AxiosResponse<Collibra.User>
-const _getUser = (client: Net.Client, id: string) => () => client.get<UserResponse>(`/user/${id}`)
+type UserResponse = AxiosResponse<Readonly<Collibra.User>>
+const _getUser = (client: Net.Client, id: string) => () => client.get<UserResponse>(`/users/${id}`)
 
 export const getUser = (client: Net.Client) => (id: string) =>
   pipe(
