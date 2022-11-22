5bc4fa2ebab75ca4cd34a3a5eca80922fd4e11bd
Aron Høyer <aron@hoyer.com>
2022-11-22 16:22:36 +0100

    refactor: Rename date field validation function

--- lib/has_invalid_fields.ts
+++
@@ -1,8 +0,0 @@
-import * as E from "fp-ts/Either"
-
-import { isValidDate } from "./is_valid_date"
-
-export const hasInvalidFields = <T = unknown>(v: T): E.Either<string[], T> => {
-  const invalidDates = Object.entries(v).filter(([, v]) => v instanceof Date && E.isLeft(isValidDate(v)))
-  return invalidDates.length > 0 ? E.left(invalidDates.map(([f]) => f)) : E.right(v)
-}
