756b47f10ba89919132afffd4437dda9a1d0c07b
Aron Høyer <aron@hoyer.com>
2022-11-22 16:35:14 +0100

    refactor: Make code easier to read (hopefully)

--- lib/collibra/filter_responsibilities_by_groups.ts
+++ lib/collibra/filter_responsibilities_by_groups.ts
@@ -1,11 +1,20 @@
-import { none, some, type Option } from "fp-ts/Option"
+import * as A from "fp-ts/Array"
+import * as E from "fp-ts/Either"
+import { Predicate } from "fp-ts/Predicate"
 
-export const filterResponsibilitiesByGroups = (
-  gs: string[],
-  rs: Collibra.Responsibility[]
-): Option<readonly Collibra.Responsibility[]> => {
-  const filtered = rs.filter((r) =>
-    gs.length > 0 ? gs.includes(r.role.name) && r.owner.resourceType === "User" : r.owner.resourceType === "User"
-  )
-  return filtered.length === 0 ? none : some(filtered)
-}
+type CurriedPredicate<A, B> = (a: A) => Predicate<B>
+
+const filterPredicate: CurriedPredicate<string[], Collibra.Responsibility> = (groups) => (responsibility) =>
+  groups.length > 0
+    ? groups.includes(responsibility.role.name) && responsibility.owner.resourceType === "User"
+    : responsibility.owner.resourceType === "User"
+
+export const filterResponsibilitiesByGroups =
+  (groups: string[]) =>
+  (responsibilities: Collibra.Responsibility[]): E.Either<Error, Collibra.Responsibility[]> => {
+    const filtered = A.filter(filterPredicate(groups))(responsibilities)
+
+    return filtered.length === 0
+      ? E.left(new Error(`No responsibilities found in group(s) ${groups.join(", ")}`))
+      : E.right(filtered)
+  }
