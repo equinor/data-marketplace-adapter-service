a1c84d574dab910463c7df5dfcece8d69485d575
Aron Høyer <aron@hoyer.com>
2022-11-22 16:38:24 +0100

    feat: Get asset maintainers

--- GetMaintainersTrigger/index.ts
+++ GetMaintainersTrigger/index.ts
@@ -1,41 +1,18 @@
 import type { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import type { Maintainer } from "@equinor/data-marketplace-models/types/Maintainer"
+import { Maintainer } from "@equinor/data-marketplace-models/types/Maintainer"
 import { AxiosError } from "axios"
-import * as E from "fp-ts/Either"
+import * as A from "fp-ts/Array"
 import * as TE from "fp-ts/TaskEither"
 import { pipe } from "fp-ts/function"
 
+import { maintainerAdapter } from "../lib/adapters/maintainer_adapter"
 import { getAssetResponsibilities } from "../lib/collibra/client/get_asset_responsibilities"
-import { getUser } from "../lib/collibra/client/get_user"
+import { getRolesByNames } from "../lib/collibra/client/get_roles_by_names"
+import { getUsersByIdBatch } from "../lib/collibra/client/get_users_by_id_batch"
 import { makeCollibraClient } from "../lib/collibra/client/make_collibra_client"
 import { filterResponsibilitiesByGroups } from "../lib/collibra/filter_responsibilities_by_groups"
-import { maintainerAdapter } from "../lib/collibra/maintainer_adapter"
 import { isErrorResult } from "../lib/net/is_error_result"
 import { makeResult } from "../lib/net/make_result"
-
-const getUsersByResponsibilities =
-  (client: Net.Client) =>
-  (responsibilities: readonly Collibra.Responsibility[]): TE.TaskEither<Error, readonly Collibra.User[]> => {
-    return pipe(
-      responsibilities.map((r) => getUser(client)(r.owner.id)),
-      TE.sequenceArray
-    )
-  }
-
-const mapUsersToMaintainers = (
-  users: readonly Collibra.User[],
-  responsibilities: readonly Collibra.Responsibility[]
-): E.Either<Error, readonly Maintainer[]> => {
-  return pipe(
-    users.map((u) =>
-      maintainerAdapter(
-        responsibilities.find((r) => r.owner.id === u.id),
-        u
-      )
-    ),
-    E.sequenceArray
-  )
-}
 
 const GetMaintainersTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   const collibraClient = makeCollibraClient({ headers: { authorization: req.headers.authorization } })
@@ -44,27 +21,48 @@ const GetMaintainersTrigger: AzureFunction = async function (context: Context, r
 
   const res = await pipe(
     getAssetResponsibilities(collibraClient)(id),
-    TE.chain((r) =>
-      TE.fromOption(() => new Error(`Responsibilities by groups [${groups.join(", ")}] was None`))(
-        filterResponsibilitiesByGroups(groups, r)
+    TE.map((responsibilities) => TE.fromEither(filterResponsibilitiesByGroups(groups)(responsibilities))),
+    TE.flatten,
+    TE.bindTo("responsibilities"),
+    // get users by ids
+    TE.bind("users", ({ responsibilities }) =>
+      pipe(
+        responsibilities,
+        A.map((responsibility) => responsibility.owner.id),
+        getUsersByIdBatch(collibraClient)
       )
     ),
-    TE.bindTo("responsibilities"),
-    TE.bind("users", ({ responsibilities }) => getUsersByResponsibilities(collibraClient)(responsibilities)),
-    TE.chain(({ responsibilities, users }) => TE.fromEither(mapUsersToMaintainers(users, responsibilities))),
+    // get roles by names
+    TE.bind("roles", ({ responsibilities }) =>
+      pipe(
+        responsibilities,
+        A.map((responsibility) => responsibility.role.name),
+        getRolesByNames(collibraClient)
+      )
+    ),
+    // adapt maintainers from roles and users
+    TE.chain(({ responsibilities, roles, users }) =>
+      pipe(
+        responsibilities,
+        A.map((responsibility) => {
+          const role = A.findFirst<Collibra.Role>((role) => role.id === responsibility.role.id)(roles)
+          const user = A.findFirst<Collibra.User>((user) => user.id === responsibility.owner.id)(users)
+          return TE.fromEither(maintainerAdapter(user)(role))
+        }),
+        TE.sequenceArray
+      )
+    ),
     TE.match(
-      (err: AxiosError) => makeResult<Maintainer[], AxiosError>(err.response?.status ?? 500, err),
-      (m: Maintainer[]) => makeResult<Maintainer[], AxiosError>(200, m)
+      (err: AxiosError) => makeResult<readonly Maintainer[], AxiosError>(err.response?.status ?? 500, err),
+      (maintainers) => makeResult<readonly Maintainer[], AxiosError>(200, maintainers)
     )
   )()
 
-  if (isErrorResult(res)) {
-    context.log(res.value)
-  }
-
   context.res = {
     status: res.status,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(isErrorResult(res) ? { error: (res.value as Error).message } : res.value),
   }
 }
