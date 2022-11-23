e8e777fb476a0d644f7404c8de6900308465b57b
Aron Høyer <aron@hoyer.com>
2022-11-23 12:35:26 +0100

    refactor: Move User object creation outside pipe

--- lib/adapters/user_adapter.ts
+++ lib/adapters/user_adapter.ts
@@ -4,15 +4,15 @@ import { pipe } from "fp-ts/function"
 
 import { hasInvalidDateFields } from "../has_invalid_date_fields"
 
-export const userAdapter = (cu: Collibra.User): E.Either<string, User> =>
-  pipe(
-    {
-      createdAt: new Date(cu.createdOn),
-      email: cu.emailAddress.toLowerCase(),
-      firstName: cu.firstName,
-      id: cu.id,
-      lastName: cu.lastName,
-      updatedAt: new Date(cu.lastModifiedOn),
-    } as User,
-    hasInvalidDateFields
-  )
+export const userAdapter = (collibraUser: Collibra.User): E.Either<string, User> => {
+  const user: User = {
+    createdAt: new Date(collibraUser.createdOn),
+    email: collibraUser.emailAddress.toLowerCase(),
+    firstName: collibraUser.firstName,
+    id: collibraUser.id,
+    lastName: collibraUser.lastName,
+    updatedAt: new Date(collibraUser.lastModifiedOn),
+  }
+
+  return pipe(user, hasInvalidDateFields)
+}
