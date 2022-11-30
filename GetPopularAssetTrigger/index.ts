fd97d32363de33f0a8f3f6b14b5f5c2f0f8e5c52
Aron Høyer <aron@hoyer.com>
2022-11-30 12:27:34 +0100

    feat: Recursively get popular assets till we have an array of length `limit`

--- GetPopularAssetTrigger/index.ts
+++ GetPopularAssetTrigger/index.ts
@@ -1,15 +1,14 @@
 import type { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { Asset } from "@equinor/data-marketplace-models"
+import type { AxiosError } from "axios"
+import * as E from "fp-ts/Either"
 import * as TE from "fp-ts/TaskEither"
 import { pipe } from "fp-ts/lib/function"
 
-import { assetAdapter } from "../lib/collibra/asset_adapter"
-import { getAssetAttributes } from "../lib/collibra/client/get_asset_attributes"
-import { getAssetByID } from "../lib/collibra/client/get_asset_by_id"
-import { getMostViewed } from "../lib/collibra/client/get_most_viewed"
-import { getProductType } from "../lib/collibra/client/get_product_type"
+import { getAssetTypeByName } from "../lib/collibra/client/get_asset_type_by_name"
 import { getStatusByName } from "../lib/collibra/client/get_status_id"
 import { makeCollibraClient } from "../lib/collibra/client/make_collibra_client"
+import { AssetWithViews, getMostViewedDataProducts } from "../lib/collibra/get_most_viewed_data_products"
+import { isErrorResult } from "../lib/net/is_error_result"
 import { makeResult } from "../lib/net/make_result"
 
 const GetPopularAssets: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
@@ -17,37 +16,35 @@ const GetPopularAssets: AzureFunction = async function (context: Context, req: H
     headers: { authorization: req.headers.authorization },
   })
 
-  const { id } = context.bindingData
   const limit = Number.isNaN(Number(req.query.limit)) ? 10 : Number(req.query.limit)
-  const offset = 0
 
   const res = await pipe(
     getStatusByName(collibraClient)("Approved"),
     TE.bindTo("approvedStatus"),
-    TE.bind("dataProductType", () => getProductType(collibraClient)("Data Product")),
-    // make next line recursive
-    TE.bind("navStat", () => getMostViewed(collibraClient)(limit, offset)),
-    TE.bind("assets", ({ navStat }) =>
-      pipe(
-        navStat.map((stat) => getAssetByID(collibraClient)(stat.assetId)),
-        TE.sequenceArray
+    TE.bind("dataProductType", () => getAssetTypeByName(collibraClient)("Data Product")),
+    TE.chain(({ approvedStatus, dataProductType }) =>
+      TE.tryCatch(
+        () => getMostViewedDataProducts(collibraClient)([], limit, 0, approvedStatus.id, dataProductType.id),
+        E.toError
       )
     ),
-
-    TE.bind("attributes", ({ assets }) =>
-      pipe(
-        assets.map((asset) => getAssetAttributes(collibraClient)(asset.id)),
-        TE.sequenceArray
-      )
-    ),
-
-    TE.map(({ assets, attributes }) => assets.map((asset, i) => ({ ...asset, attributes: attributes[i] }))),
-    TE.map((assets) => assets.map(assetAdapter)),
-    TE.match<Error, Net.Result<Asset[], Error>, Asset[]>(
-      (err) => makeResult<Asset[], Error>(500, err),
-      (assets) => makeResult<Asset[], Error>(200, assets)
+    TE.map((assets) => assets.sort((a, b) => (a.views > b.views ? -1 : 1))),
+    TE.match<AxiosError, Net.Result<AssetWithViews[], AxiosError>, AssetWithViews[]>(
+      (err) => {
+        console.error("[GetPopularAssets]", err)
+        return makeResult<AssetWithViews[], AxiosError>(err.response?.status ?? 500, err)
+      },
+      (assets) => makeResult<AssetWithViews[], AxiosError>(200, assets)
     )
-  )
+  )()
+
+  context.res = {
+    status: res.status,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(isErrorResult(res) ? { error: (res.value as Error).message } : res.value),
+  }
 }
 
 export default GetPopularAssets
