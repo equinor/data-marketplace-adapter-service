cdc9ba2cf7dc7b96e7c5e728b0e6420724fda459
Aron Høyer <aron@hoyer.com>
2022-12-01 12:26:48 +0100

    feat: Log errors in match

--- GetTermsAndConditionsTrigger/index.ts
+++ GetTermsAndConditionsTrigger/index.ts
@@ -8,11 +8,13 @@ import { getAssetAttributes } from "../lib/collibra/client/get_asset_attributes"
 import { getRightsToUseAsset } from "../lib/collibra/client/get_rights_to_use_asset"
 import { makeCollibraClient } from "../lib/collibra/client/make_collibra_client"
 import { rightsToUseAdapter } from "../lib/collibra/rights_to_use_adapter"
+import { makeLogger } from "../lib/logger"
 import { isErrorResult } from "../lib/net/is_error_result"
 import { makeResult } from "../lib/net/make_result"
 
 const GetTermsAndConditionTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  const collibraClient = makeCollibraClient(req.headers.authorization)(context.log)
+  const logger = makeLogger(context.log)
+  const collibraClient = makeCollibraClient(req.headers.authorization)(logger)
 
   const { id } = context.bindingData
 
@@ -24,7 +26,10 @@ const GetTermsAndConditionTrigger: AzureFunction = async function (context: Cont
       rightsToUseAdapter({ ...collibraRtuAsset, attributes: collibraRtuAttrs })
     ),
     TE.match<AxiosError, Net.Result<RightsToUse, AxiosError>, RightsToUse>(
-      (err) => makeResult<RightsToUse, AxiosError>(err.response?.status ?? 500, err),
+      (err) => {
+        logger.error(err)
+        return makeResult<RightsToUse, AxiosError>(err.response?.status ?? 500, err)
+      },
       (asset) => makeResult<RightsToUse, AxiosError>(200, asset)
     )
   )()
