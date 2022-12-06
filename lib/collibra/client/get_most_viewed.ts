914e09f0578b2baffdb491ff18e58155e96f1263
Aron Høyer <aron@hoyer.com>
2022-12-06 11:04:53 +0100

    refactor: Use Get utilitiy function

--- lib/collibra/client/get_most_viewed.ts
+++ lib/collibra/client/get_most_viewed.ts
@@ -3,13 +3,15 @@ import * as E from "fp-ts/Either"
 import * as TE from "fp-ts/TaskEither"
 import { pipe } from "fp-ts/lib/function"
 
+import { Get } from "../../net/get"
+
 type NavStatsResponse = AxiosResponse<Collibra.PagedNavigationStatisticResponse>
 
 export const getMostViewed = (client: Net.Client) => (limit: number, offset: number) =>
   pipe(
     TE.tryCatch(
       () =>
-        client.get<NavStatsResponse>("/navigation/most_viewed", {
+        Get<NavStatsResponse>(client)("/navigation/most_viewed", {
           params: new URLSearchParams({
             limit: String(limit),
             offset: String(offset),
--- lib/collibra/client/get_status_id.ts
+++ lib/collibra/client/get_status_id.ts
@@ -3,8 +3,10 @@ import * as E from "fp-ts/Either"
 import * as TE from "fp-ts/TaskEither"
 import { pipe } from "fp-ts/lib/function"
 
+import { Get } from "../../net/get"
+
 export const getStatusByName = (client: Net.Client) => (name: string) =>
   pipe(
-    TE.tryCatch(() => client.get<AxiosResponse<Collibra.Status>>(`/statuses/name/${name}`), E.toError),
+    TE.tryCatch(() => Get<AxiosResponse<Collibra.Status>>(client)(`/statuses/name/${name}`), E.toError),
     TE.map(({ data }) => data)
   )
