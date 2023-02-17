6f3265e00fae3864b9cdde967c814a883bd16aa3
Aron Høyer <aron@hoyer.com>
2023-02-17 12:46:54 +0100

    refactor: Move assets request to own file

--- GetAssetsTrigger/index.ts
+++ GetAssetsTrigger/index.ts
@@ -1,6 +1,5 @@
 import type { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { Asset } from "@equinor/data-marketplace-models"
-import { AxiosError } from "axios"
 import * as A from "fp-ts/Array"
 import * as TE from "fp-ts/TaskEither"
 import { pipe } from "fp-ts/lib/function"
@@ -8,14 +7,13 @@ import { pipe } from "fp-ts/lib/function"
 import { assetAdapter } from "../lib/collibra/asset_adapter"
 import { getAssetAttributes } from "../lib/collibra/client/get_asset_attributes"
 import { getAssetTypeByName } from "../lib/collibra/client/get_asset_type_by_name"
+import { getAssets } from "../lib/collibra/client/get_assets"
 import { getStatusByName } from "../lib/collibra/client/get_status_id"
 import { makeCollibraClient } from "../lib/collibra/client/make_collibra_client"
 import { makeLogger } from "../lib/logger"
 import { NetError } from "../lib/net/NetError"
-import { Get } from "../lib/net/get"
 import { isErrorResult } from "../lib/net/is_error_result"
 import { makeResult } from "../lib/net/make_result"
-import { toNetErr } from "../lib/net/to_net_err"
 
 const GetAssetsTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   const logger = makeLogger(context.log)
@@ -28,19 +26,13 @@ const GetAssetsTrigger: AzureFunction = async function (context: Context, req: H
     TE.bindTo("approvedStatus"),
     TE.bind("dataProductType", () => getAssetTypeByName(collibraClient)("Data Product")),
     TE.bind("assets", ({ approvedStatus, dataProductType }) =>
-      TE.tryCatch(
-        async () => {
-          const { results: assets } = await Get<Collibra.PagedAssetResponse>(collibraClient)("/assets", {
-            params: new URLSearchParams({
-              statusIds: approvedStatus.id,
-              typeIds: dataProductType.id,
-              limit: limit ?? "100",
-              offset: offset ?? "0",
-            }),
-          })
-          return assets
-        },
-        (err: AxiosError) => toNetErr(err.response?.status ?? 500)(err.message)
+      getAssets(collibraClient)(
+        new URLSearchParams({
+          statusIds: approvedStatus.id,
+          typeIds: dataProductType.id,
+          limit: limit ?? "100",
+          offset: offset ?? "0",
+        })
       )
     ),
     TE.chain(({ assets }) =>
