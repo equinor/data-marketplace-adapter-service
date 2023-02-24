ef878ede3c243d651a4d1dea32674608f00d8291
Shikha Mishra <shikhamishra.jec@gmail.com>
2023-02-24 12:51:16 +0100

    Enable strict

--- lib/has_invalid_date_fields.ts
+++ lib/has_invalid_date_fields.ts
@@ -1,8 +1,8 @@
 import * as E from "fp-ts/Either"
 
 import { isValidDate } from "./is_valid_date"
-
-export const hasInvalidDateFields = <T extends () => void>(v: T): E.Either<string, T> => {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const hasInvalidDateFields = <T extends {}>(v: T): E.Either<string, T> => {
   const invalidDates = Object.entries(v).filter(([, v]) => v instanceof Date && E.isLeft(isValidDate(v)))
   return invalidDates.length > 0
     ? E.left(`Invalid date(s) in field(s) ${invalidDates.map(([f]) => f).join(", ")}`)
