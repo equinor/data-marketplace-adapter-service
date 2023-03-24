666fcb8484c9edf52a7cf78ae329b72f8f25ca89
Aron Høyer <aron@hoyer.com>
2023-03-24 15:04:26 +0100

    refactor: Use Collibra's output module to get asset data

--- lib/net/post.ts
+++ lib/net/post.ts
@@ -1,6 +1,9 @@
-export const Post = (client: Net.Client) => (url: string, opts?: Omit<Net.RequestOpts, "method">) => {
-  return client.request(url, {
-    ...opts,
-    method: "POST",
-  })
-}
+export const Post =
+  <T = unknown, B = unknown>(client: Net.Client) =>
+  (url: string, opts?: Omit<Net.RequestOpts, "method">) => {
+    return client.request<T>(url, {
+      ...opts,
+      body: opts?.body as B,
+      method: "POST",
+    })
+  }
