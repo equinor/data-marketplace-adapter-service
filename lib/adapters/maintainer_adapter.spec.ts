c790f215f02ec71c51f8ef39c300ba8ffd95103f
Aron Høyer <aron@hoyer.com>
2023-04-10 09:39:18 +0200

    test: Update tests for maintainer adapter

--- lib/adapters/maintainer_adapter.spec.ts
+++ lib/adapters/maintainer_adapter.spec.ts
@@ -1,75 +1,51 @@
 import { randomUUID } from "crypto"
 
 import * as E from "fp-ts/Either"
-import * as O from "fp-ts/Option"
 import { pipe } from "fp-ts/lib/function"
 
-import { invalidateCollibraResource } from "../testing/invalidate_collibra_resource"
+import {
+  ResponsibilityGroupUser,
+  ResponsibilityRole,
+  ResponsibilityUser,
+} from "../../GetMaintainersTrigger/lib/getResponsibilities"
 
 import { maintainerAdapter } from "./maintainer_adapter"
 
 describe("maintainerAdapter", () => {
-  let role: O.Option<Collibra.Role> = O.none
-  let user: O.Option<Collibra.User> = O.none
+  let role: ResponsibilityRole | null = null
+  let user: ResponsibilityUser | ResponsibilityGroupUser | null = null
 
   beforeEach(() => {
-    role = O.some({
-      createdBy: randomUUID(),
-      createdOn: new Date().valueOf(),
-      description: "Lorem ipsum dolor sit amet",
-      global: false,
-      id: randomUUID(),
-      lastModifiedBy: randomUUID(),
-      lastModifiedOn: new Date().valueOf(),
-      name: "Data Office Admin",
-      permissions: ["ACCESS_DATA", "ADVANCED_DATA_TYPE_ADD", "ADVANCED_DATA_TYPE_EDIT"],
-      resourceType: "Role",
-      system: false,
-    })
-    user = O.some({
-      activated: true,
-      additionalEmailAddresses: [],
-      addresses: [],
-      apiUser: false,
-      createdBy: randomUUID(),
-      createdOn: new Date().valueOf(),
-      emailAddress: "NAME@example.com",
-      enabled: true,
-      firstName: "Name",
-      gender: "UNKNOWN",
-      guestUser: false,
-      id: randomUUID(),
-      instantMessagingAccounts: [],
-      language: "en",
-      lastModifiedBy: randomUUID(),
-      lastModifiedOn: new Date().valueOf(),
-      lastName: "Nameson",
-      ldapUser: false,
-      licenseType: "CONSUMER",
-      phoneNumbers: [],
-      resourceType: "User",
-      system: false,
-      userName: "NAME@example.com",
-      userSource: "SSO",
-      websites: [],
-    })
+    role = {
+      roleCreatedAt: new Date().valueOf(),
+      roleId: randomUUID(),
+      roleName: "Role Name",
+      roleUpdatedAt: new Date().valueOf(),
+    }
+    user = {
+      userCreatedAt: new Date().valueOf(),
+      userEmail: "name@example.com",
+      userFirstName: "Name",
+      userId: randomUUID(),
+      userLastName: "Nameson",
+      userUpdatedAt: new Date().valueOf(),
+    } as ResponsibilityUser
   })
 
   afterEach(() => {
-    role = O.none
-    user = O.none
+    role = null
+    user = null
   })
 
   it("returns left path for invalid date", () => {
-    const invalidUser = pipe(user, O.map(invalidateCollibraResource))
-
-    const invalidRole = pipe(role, O.map(invalidateCollibraResource))
+    const invalidUser = pipe(user, (u) => ({ ...u, userCreatedAt: "invalid date" }))
+    const invalidRole = pipe(role, (r) => ({ ...r, roleCreatedAt: "invalid date" }))
 
     // @ts-ignore
-    expect(E.isLeft(maintainerAdapter(invalidUser)(invalidRole))).toBe(true)
+    expect(E.isLeft(maintainerAdapter({ role: invalidRole, user: invalidUser }))).toBe(true)
   })
 
   it("returns right path for valid input", () => {
-    expect(E.isRight(maintainerAdapter(user)(role))).toBe(true)
+    expect(E.isRight(maintainerAdapter({ role: role!, user: user! }))).toBe(true)
   })
 })
--- lib/adapters/maintainer_adapter.ts
+++ lib/adapters/maintainer_adapter.ts
@@ -1,10 +1,13 @@
 import { Maintainer, User } from "@equinor/data-marketplace-models"
+import * as E from "fp-ts/lib/Either"
+import { pipe } from "fp-ts/lib/function"
 
 import {
   ResponsibilityGroupUser,
   ResponsibilityRole,
   ResponsibilityUser,
 } from "../../GetMaintainersTrigger/lib/getResponsibilities"
+import { hasInvalidDateFields } from "../has_invalid_date_fields"
 
 type Responsibility = {
   user: ResponsibilityUser | ResponsibilityGroupUser
@@ -13,43 +16,47 @@ type Responsibility = {
 
 const isGroupUser = (user: object): user is ResponsibilityGroupUser => "groupUserId" in user
 
-export const maintainerAdapter = (responsibility: Responsibility): Maintainer => {
-  const user: User = {
-    createdAt: new Date(),
-    email: "",
-    firstName: "",
-    id: "",
-    lastName: "",
-    updatedAt: new Date(),
-  }
-
-  if (isGroupUser(responsibility.user)) {
-    Object.assign(user, {
-      createdAt: new Date(responsibility.user.groupUserCreatedAt),
-      email: responsibility.user.groupUserEmail?.toLowerCase(),
-      firstName: responsibility.user.groupUserFirstName,
-      id: responsibility.user.groupUserId,
-      lastName: responsibility.user.groupUserLastName,
-      updatedAt: new Date(responsibility.user.groupUserUpdatedAt),
-    } as User)
-  } else {
-    Object.assign(user, {
-      createdAt: new Date(responsibility.user.userCreatedAt),
-      email: responsibility.user.userEmail?.toLowerCase(),
-      firstName: responsibility.user.userFirstName,
-      id: responsibility.user.userId,
-      lastName: responsibility.user.userLastName,
-      updatedAt: new Date(responsibility.user.userUpdatedAt),
-    } as User)
-  }
-
-  return {
-    ...user,
-    role: {
-      createdAt: new Date(responsibility.role.roleCreatedAt),
-      id: responsibility.role.roleId,
-      name: responsibility.role.roleName,
-      updatedAt: new Date(responsibility.role.roleUpdatedAt),
-    },
-  }
-}
+export const maintainerAdapter = (responsibility: Responsibility): E.Either<string, Maintainer> =>
+  pipe(
+    E.of(responsibility),
+    E.map((r) =>
+      pipe(
+        E.of(isGroupUser(r.user)),
+        E.map((b) =>
+          b
+            ? ({
+                createdAt: new Date((responsibility.user as ResponsibilityGroupUser).groupUserCreatedAt),
+                email: (responsibility.user as ResponsibilityGroupUser).groupUserEmail?.toLowerCase(),
+                firstName: (responsibility.user as ResponsibilityGroupUser).groupUserFirstName,
+                id: (responsibility.user as ResponsibilityGroupUser).groupUserId,
+                lastName: (responsibility.user as ResponsibilityGroupUser).groupUserLastName,
+                updatedAt: new Date((responsibility.user as ResponsibilityGroupUser).groupUserUpdatedAt),
+              } as User)
+            : ({
+                createdAt: new Date((responsibility.user as ResponsibilityUser).userCreatedAt),
+                email: (responsibility.user as ResponsibilityUser).userEmail?.toLowerCase(),
+                firstName: (responsibility.user as ResponsibilityUser).userFirstName,
+                id: (responsibility.user as ResponsibilityUser).userId,
+                lastName: (responsibility.user as ResponsibilityUser).userLastName,
+                updatedAt: new Date((responsibility.user as ResponsibilityUser).userUpdatedAt),
+              } as User)
+        )
+      )
+    ),
+    E.flattenW,
+    E.map(hasInvalidDateFields),
+    E.flattenW,
+    E.map(
+      (u) =>
+        ({
+          ...u,
+          role: {
+            createdAt: new Date(responsibility.role.roleCreatedAt),
+            id: responsibility.role.roleId,
+            name: responsibility.role.roleName,
+            updatedAt: new Date(responsibility.role.roleUpdatedAt),
+          },
+        } as Maintainer)
+    ),
+    E.mapLeft(() => "Unable to process responsibility data from Collibra")
+  )
