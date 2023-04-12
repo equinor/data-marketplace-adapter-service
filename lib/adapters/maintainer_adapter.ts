a655f0651f9796fc3d626225050d6c5cf6f1dfa9
Aron Høyer <aron@hoyer.com>
2023-04-12 12:44:19 +0200

    refactor: Improve maintainer adapter readability

--- lib/adapters/maintainer_adapter.ts
+++ lib/adapters/maintainer_adapter.ts
@@ -16,24 +16,19 @@ type Responsibility = {
   role: ResponsibilityRole
 }
 
-const makeMaintainerObject = (role: ResponsibilityRole) => (user: User) =>
-  ({
-    ...user,
-    role: {
-      createdAt: new Date(role.roleCreatedAt),
-      id: role.roleId,
-      name: role.roleName,
-      updatedAt: new Date(role.roleUpdatedAt),
-    },
-  } as Maintainer)
+const makeMaintainerObject =
+  (role: ResponsibilityRole) =>
+  (user: User): E.Either<string, Maintainer> =>
+    pipe(
+      E.of({
+        createdAt: new Date(role.roleCreatedAt),
+        id: role.roleId,
+        name: role.roleName,
+        updatedAt: new Date(role.roleUpdatedAt),
+      }),
+      E.chain(hasInvalidDateFields),
+      E.map((r) => ({ ...user, role: r }))
+    )
 
 export const maintainerAdapter = (responsibility: Responsibility): E.Either<string, Maintainer> =>
-  pipe(
-    E.of(responsibility),
-    E.map((r) => userAdapter(r.user)),
-    E.flattenW,
-    hasInvalidDateFields,
-    E.flattenW,
-    E.map(makeMaintainerObject(responsibility.role)),
-    E.mapLeft(() => "Unable to process responsibility data from Collibra")
-  )
+  pipe(userAdapter(responsibility.user), E.chain(makeMaintainerObject(responsibility.role)))
