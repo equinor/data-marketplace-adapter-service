da378b263f9e15d046fb709f78dd58aa48ebc976
Aron Høyer <aron@hoyer.com>
2023-04-12 09:41:19 +0200

    refactor: Improve maintainer adapter readability

--- lib/adapters/maintainer_adapter.ts
+++ lib/adapters/maintainer_adapter.ts
@@ -9,54 +9,31 @@ import {
 } from "../../GetMaintainersTrigger/lib/getResponsibilities"
 import { hasInvalidDateFields } from "../has_invalid_date_fields"
 
+import { userAdapter } from "./user_adapter"
+
 type Responsibility = {
   user: ResponsibilityUser | ResponsibilityGroupUser
   role: ResponsibilityRole
 }
 
-const isGroupUser = (user: object): user is ResponsibilityGroupUser => "groupUserId" in user
+const makeMaintainerObject = (role: ResponsibilityRole) => (user: User) =>
+  ({
+    ...user,
+    role: {
+      createdAt: new Date(role.roleCreatedAt),
+      id: role.roleId,
+      name: role.roleName,
+      updatedAt: new Date(role.roleUpdatedAt),
+    },
+  } as Maintainer)
 
 export const maintainerAdapter = (responsibility: Responsibility): E.Either<string, Maintainer> =>
   pipe(
     E.of(responsibility),
-    E.map((r) =>
-      pipe(
-        E.of(isGroupUser(r.user)),
-        E.map((b) =>
-          b
-            ? ({
-                createdAt: new Date((responsibility.user as ResponsibilityGroupUser).groupUserCreatedAt),
-                email: (responsibility.user as ResponsibilityGroupUser).groupUserEmail?.toLowerCase(),
-                firstName: (responsibility.user as ResponsibilityGroupUser).groupUserFirstName,
-                id: (responsibility.user as ResponsibilityGroupUser).groupUserId,
-                lastName: (responsibility.user as ResponsibilityGroupUser).groupUserLastName,
-                updatedAt: new Date((responsibility.user as ResponsibilityGroupUser).groupUserUpdatedAt),
-              } as User)
-            : ({
-                createdAt: new Date((responsibility.user as ResponsibilityUser).userCreatedAt),
-                email: (responsibility.user as ResponsibilityUser).userEmail?.toLowerCase(),
-                firstName: (responsibility.user as ResponsibilityUser).userFirstName,
-                id: (responsibility.user as ResponsibilityUser).userId,
-                lastName: (responsibility.user as ResponsibilityUser).userLastName,
-                updatedAt: new Date((responsibility.user as ResponsibilityUser).userUpdatedAt),
-              } as User)
-        )
-      )
-    ),
+    E.map((r) => userAdapter(r.user)),
     E.flattenW,
-    E.map(hasInvalidDateFields),
+    hasInvalidDateFields,
     E.flattenW,
-    E.map(
-      (u) =>
-        ({
-          ...u,
-          role: {
-            createdAt: new Date(responsibility.role.roleCreatedAt),
-            id: responsibility.role.roleId,
-            name: responsibility.role.roleName,
-            updatedAt: new Date(responsibility.role.roleUpdatedAt),
-          },
-        } as Maintainer)
-    ),
+    E.map(makeMaintainerObject(responsibility.role)),
     E.mapLeft(() => "Unable to process responsibility data from Collibra")
   )
---
+++ lib/adapters/user_adapter.ts
@@ -0,0 +1,31 @@
+import { User } from "@equinor/data-marketplace-models"
+import * as E from "fp-ts/lib/Either"
+import { pipe } from "fp-ts/lib/function"
+
+import { ResponsibilityGroupUser, ResponsibilityUser } from "../../GetMaintainersTrigger/lib/getResponsibilities"
+
+const isGroupUser = (user: object): user is ResponsibilityGroupUser => "groupUserId" in user
+
+export const userAdapter = (user: ResponsibilityUser | ResponsibilityGroupUser) =>
+  pipe(
+    E.of(isGroupUser(user)),
+    E.map((b) =>
+      b
+        ? ({
+            createdAt: new Date((user as ResponsibilityGroupUser).groupUserCreatedAt),
+            email: (user as ResponsibilityGroupUser).groupUserEmail?.toLowerCase(),
+            firstName: (user as ResponsibilityGroupUser).groupUserFirstName,
+            id: (user as ResponsibilityGroupUser).groupUserId,
+            lastName: (user as ResponsibilityGroupUser).groupUserLastName,
+            updatedAt: new Date((user as ResponsibilityGroupUser).groupUserUpdatedAt),
+          } as User)
+        : ({
+            createdAt: new Date((user as ResponsibilityUser).userCreatedAt),
+            email: (user as ResponsibilityUser).userEmail?.toLowerCase(),
+            firstName: (user as ResponsibilityUser).userFirstName,
+            id: (user as ResponsibilityUser).userId,
+            lastName: (user as ResponsibilityUser).userLastName,
+            updatedAt: new Date((user as ResponsibilityUser).userUpdatedAt),
+          } as User)
+    )
+  )
