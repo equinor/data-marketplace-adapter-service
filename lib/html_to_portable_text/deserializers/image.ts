fb8ea7b060c91d59985506a9b1854e774e84c0f9
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-04-13 15:51:04 +0200

    ⬆️ Upgrade @sanity packages to v3

--- lib/html_to_portable_text/deserializers/image.ts
+++
@@ -1,9 +0,0 @@
-export const deserializeImage = (el: HTMLElement, _next: () => void, block: any) => {
-  return block({
-    _type: "image",
-    src: el.getAttribute("src"),
-    alt: el.getAttribute("alt"),
-    height: el.getAttribute("height"),
-    width: el.getAttribute("width"),
-  })
-}
