589db8b0803a2e2c1debbde53cf6c2f30abdb6ca
Aron Høyer <aron@hoyer.com>
2023-04-19 09:33:02 +0200

    refactor: Remove unused code (#64)
    * refactor: Move asset adapter
    
    * refactor: Remove unused collibra client funcitons
    
    * refactor: Remove unused endpoint

--- lib/collibra/client/get_asset_by_id.ts
+++
@@ -1,9 +0,0 @@
-import { HttpStatusCode } from "axios"
-import * as TE from "fp-ts/TaskEither"
-
-import { Get } from "../../net/get"
-import { toNetError } from "../../net/to_net_err"
-
-export const getAssetByID = (client: Net.Client) => (id: string) =>
-  TE.tryCatch(() => Get<Collibra.Asset>(client)(`/assets/${id}`), toNetError(HttpStatusCode.InternalServerError))
-
--- lib/collibra/client/get_user.ts
+++
@@ -1,8 +0,0 @@
-import { HttpStatusCode } from "axios"
-import * as TE from "fp-ts/TaskEither"
-
-import { Get } from "../../net/get"
-import { toNetError } from "../../net/to_net_err"
-
-export const getUser = (client: Net.Client) => (id: string) =>
-  TE.tryCatch(() => Get<Collibra.User>(client)(`/users/${id}`), toNetError(HttpStatusCode.InternalServerError))
